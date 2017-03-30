2efec5516883498ebdb3b5d2dfdc80576396b18c
dtiarks <daniel.tiarks@gmail.com>
2017-03-30 08:19:28 +0200

    Disabled huber loss and added dones.

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -318,7 +318,7 @@ if __name__ == '__main__':
             "discount":0.99,
             "learningrate_actor":1e-4,
             "learningrate_critic":1e-3,
-            "huberloss":True,
+            "huberloss":False,
             "gradientmomentum":0.99,
             "sqgradientmomentum":0.95,
             "mingradientmomentum":0.00,
